8b63a41004d014f6ccb3ac87a59db790f4cd86e1
POiSON <POiSON@QUiET>
2015-07-24 16:53:59 +0300

    перезапись

--- Git-test-project/src/by/zti/test/Main.java
+++ Git-test-project/src/by/zti/test/Main.java
@@ -3,6 +3,6 @@ package by.zti.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!!");
 	}
 }
